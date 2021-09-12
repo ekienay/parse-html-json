a5b2b4470fc9aaa70322259a4782ed169fc747c8
ekienay <ekienay@gmail.com>
2021-09-12 00:12:47 -0400

    json parse for futered restapi

--- src/main/java/com/def/Run.java
+++ src/main/java/com/def/Run.java
@@ -1,32 +1,17 @@
 package com.def;
 
+import com.def.module.JSONParser;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
-import org.json.simple.parser.ParseException;
 
-import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 
 public class Run {
     public static void main(String[] args) throws MalformedURLException {
-        String result = parseJSON(new URL("http://worldtimeapi.org/api/timezone/America/Santiago"));
+        String result = JSONParser.parseJSON(new URL("http://worldtimeapi.org/api/timezone/America/Santiago"));
         JSONObject obj = (JSONObject) JSONValue.parse(result);
         System.out.println("Date and time: " + obj.get("datetime"));
     }
-
-    public static String parseJSON(URL url){
-        StringBuilder sb = new StringBuilder();
-        try(BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream()))){
-            String line;
-            while ((line = bf.readLine()) != null){
-                sb.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return sb.toString();
-    }
-
 }
---
+++ src/main/java/com/def/module/JSONParser.java
@@ -0,0 +1,22 @@
+package com.def.module;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class JSONParser {
+
+    public static String parseJSON(URL url){
+        StringBuilder sb = new StringBuilder();
+        try(BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream()))){
+            String line;
+            while ((line = bf.readLine()) != null){
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+}
